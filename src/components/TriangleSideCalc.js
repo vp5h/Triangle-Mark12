3ea14540c6f2784879355e249d873a509c2864d5
Pravesh Vyas <praveshvyas.18@gmail.com>
2021-08-16 11:04:26 +0530

    -fix Logic@AngleCalc

--- src/components/TriangleSideCalc.js
+++ src/components/TriangleSideCalc.js
@@ -7,6 +7,7 @@ function TriangleCalcHandler(){
     const [angleThree, SetAngleThree]= useState("") 
     const [sum, SetSum]= useState("180") 
     const [mes, Setmes]= useState("")
+    const [diff, setDiff] = useState("")
       const angleOneHandler = (event)=>{
         var  angleOne = Number(event.target.value)
         SetAngleOne(angleOne)
@@ -25,26 +26,31 @@ function TriangleCalcHandler(){
       }
     
       function AngleCalcHandler(){
-        if((angleOne>0 && angleTwo>0 && SetAngleThree>0)|| (angleOne<178 && angleTwo<178 && angleThree<178)){
+        if((angleOne>0 || angleTwo>0 || angleThree>0)|| (angleOne<179 && angleTwo<179 && angleThree<179)){
        var sum = angleOne + angleTwo + angleThree;
        console.log(sum)
         SetSum(sum)
         var diff= 180 - Number(sum);
         console.log(diff)
         if(sum>180){
-          Setmes("Given Angles do not form a Trinagle, You have gone long by")
+          Setmes("Given Angles do not form a Trinagle, You have gone long by ")
+          setDiff(Math.abs(diff))
         }else if(sum===180){
-          Setmes("Looks like You Found a Perfect Triangle of")
+          Setmes("Looks like You Found a Perfect Triangle ")
+          setDiff("")
           
+        }else if(sum<0){
+          Setmes("Make sure you have a Positive Sum")
+          setDiff("")
           
         }else{
-           Setmes("Given Angles do not form a Triangle, You feel short by")
-         
+           Setmes("Given Angles do not form a Triangle, You feel short by ")
+           setDiff(diff)
         }
       }else{
         Setmes("Make Sure You Enter Positive angles and They are Below 178 to Form a Triangle")
         
-        
+        setDiff("")
       }
       }
       return(
@@ -54,7 +60,8 @@ function TriangleCalcHandler(){
         <input type="number" onChange={angleTwoHandler} placeholder="Angle Two"></input>
         <input type="number" onChange={angleThreeHandler} placeholder="Angle Three"></input>
         <br></br><button onClick={AngleCalcHandler}>Check Your angles </button>
-        <br></br> {angleOne}<br></br>{angleTwo}<br></br>{angleThree}<br/> {mes} {Math.abs(180-sum)}
+        {/* <br></br> {angleOne}<br></br>{angleTwo}<br></br>{angleThree} */}
+        <br></br><br></br> <br/> {mes} {diff}
     
     </div>
     
